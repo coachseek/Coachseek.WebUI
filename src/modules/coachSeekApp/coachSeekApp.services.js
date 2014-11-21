001b08fa077c7418e1fc329df64ed6e408f39abb
Reade Lobdill <readelobdill@gmail.com>
2014-11-21 20:23:55 +1300

    add fake data
    so we can see stuff happen. also added delay in resolving promise for loading.
    
    #465

--- src/modules/coachSeekApp/coachSeekApp.services.js
+++ src/modules/coachSeekApp/coachSeekApp.services.js
@@ -1,40 +1,157 @@
 /* Services */
 
 angular.module('coachSeekApp.services', []).
-  factory('coachSeekAPIService', ['$http', function($http) {
+  factory('coachSeekAPIService', ['$http', '$q', '$timeout', function($http, $q, $timeout) {
 
     var coachSeekAPI = {};
 
-    coachSeekAPI.getCoaches = function() {
+    var startTime = new Date()
+    startTime.setHours(9)
+    startTime.setMinutes(0);
+    var finishTime = new Date()
+    finishTime.setHours(17)
+    finishTime.setMinutes(0);		
+
+    coachSeekAPI.getCoaches = function(businessId) {
       // return $http({
       //   method: 'GET', 
       //   url: 'https://api.coachseek.com/api/Coaches',
       //   params: {businessId: '@businessId'}
       // });
+		this.deferred = $q.defer();
+		var self = this;
+		$timeout(function(){
+		   self.deferred.resolve({});
+		}, _.random(500, 1500));
+		return this.deferred.promise;
     };
 
-    coachSeekAPI.getCoach = function(userId){
-    	// return $http({
-    	//   method: 'GET', 
-    	//   url: 'https://api.coachseek.com/api/Coaches',
-    	//   params: {businessId: '@businessId', coachId: '@coachId'}
-    	// });
-    }
+  //   coachSeekAPI.getCoach = function(businessId, coachId){
+  //   	// return $http({
+  //   	//   method: 'GET', 
+  //   	//   url: 'https://api.coachseek.com/api/Coaches',
+  //   	//   params: {businessId: '@businessId', coachId: '@coachId'}
+  //   	// });
 
-    coachSeekAPI.saveCoach = function(){
+
+
+		// this.deferred = $q.defer();
+		// var self = this;
+		// $timeout(function(){
+		//    self.deferred.resolve({
+		// 		businessId: "8786bcd0-3b14-4f7b-92db-198527a5b949",
+		// 		id: null,
+		// 		firstName: "Koot",
+		// 		lastName: "Stains",
+		// 		email: "n.h@example.com",
+		// 		phone: "021 99 88 77",
+		// 		workingHours: {
+		// 			monday: { 
+		// 				isAvailable: true,
+		// 				startTime: startTime,
+		// 				finishTime: finishTime
+		// 			},
+		// 			tuesday: {
+		// 				isAvailable: true,
+		// 				startTime: startTime,
+		// 				finishTime: finishTime
+		// 			}, 
+		// 			wednesday: {
+		// 				isAvailable: true,
+		// 				startTime: startTime,
+		// 				finishTime: finishTime
+		// 			},
+		// 			thursday: {
+		// 				isAvailable: true,
+		// 				startTime: startTime,
+		// 				finishTime: finishTime
+		// 			},
+		// 			friday: {
+		// 				isAvailable: true,
+		// 				startTime: startTime,
+		// 				finishTime: finishTime
+		// 			},
+		// 			saturday: {
+		// 				isAvailable: false,
+		// 				startTime: startTime, 
+		// 				finishTime: finishTime
+		// 			}, 
+		// 			sunday: {
+		// 				isAvailable: false,
+		// 				startTime: startTime, 
+		// 				finishTime: finishTime
+		// 			}
+		// 		}
+		// 	});
+		// }, _.random(500, 1500));
+  // 		return this.deferred.promise;
+  //   }
+
+    coachSeekAPI.saveCoach = function(businessId, coachId){
     	// return $http({
     	//   method: 'POST', 
     	//   url: 'https://api.coachseek.com/api/Coaches',
     	//   params: {businessId: '@businessId', coachId: '@coachId'}
     	// });
+		var deferred = $q.defer();
+		deferred.resolve("DATA");
+		return deferred.promise;
     };
 
     coachSeekAPI.createCoach = function(){
     	// return $http({
     	//   method: 'PUT', 
     	//   url: 'https://api.coachseek.com/api/Coaches',
-    	//   params: {businessId: '@businessId', coachId: '@coachId'}
+    	//   params: {businessId: '@businessId'}
     	// });
+
+		var deferred = $q.defer();
+  		deferred.resolve({
+					businessId: "8786bcd0-3b14-4f7b-92db-198527a5b949",
+					id: null,
+					firstName: "NEWEST",
+					lastName: "USER",
+					email: "aaron.smith@example.com",
+					phone: "021 99 88 77",
+					workingHours: {
+						monday: { 
+							isAvailable: true,
+							startTime: startTime,
+							finishTime: finishTime
+						},
+						tuesday: {
+							isAvailable: true,
+							startTime: startTime,
+							finishTime: finishTime
+						}, 
+						wednesday: {
+							isAvailable: true,
+							startTime: startTime,
+							finishTime: finishTime
+						},
+						thursday: {
+							isAvailable: true,
+							startTime: startTime,
+							finishTime: finishTime
+						},
+						friday: {
+							isAvailable: true,
+							startTime: startTime,
+							finishTime: finishTime
+						},
+						saturday: {
+							isAvailable: false,
+							startTime: startTime, 
+							finishTime: finishTime
+						}, 
+						sunday: {
+							isAvailable: false,
+							startTime: startTime, 
+							finishTime: finishTime
+						}
+					}
+				});
+  		return deferred.promise;
     };
 
     return coachSeekAPI;
