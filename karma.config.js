3f32eefe0ac5bc8955faf04f2bde48d43704513c
Reade Lobdill <readelobdill@gmail.com>
2014-12-04 14:10:48 -0800

    #351 stop preprocessing scripts to make debugging easier

--- karma.config.js
+++ karma.config.js
@@ -38,9 +38,9 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'src/js/scripts.js': ['coverage']
-    },
+    // preprocessors: {
+    //   'src/js/scripts.js': ['coverage']
+    // },
 
 
     // test results reporter to use
