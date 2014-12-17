2f24e7d304a469ddca85f790eb104ff6f69c19e0
Reade Lobdill <readelobdill@gmail.com>
2014-12-17 13:44:57 -0800

    #216 add tests for repeatSelector directive

--- src/modules/businessSetup/tests/repeatSelector.spec.js
+++ src/modules/businessSetup/tests/repeatSelector.spec.js
@@ -30,45 +30,119 @@ describe('repeatSelector directive', function(){
         expect($repeatFrequencySelected.text()).to.equal('week');
     });
     describe('when changing the sessionCount', function(){
-        it('should set the sessionCount to the scope', function(){
+        beforeEach(function(){
             $sessionCount = $testRegion.find('.session-count');
             $sessionCount.get(0).click();
 
-            $testRegion.find('input').val(130)
-            $testRegion.find('button[type=submit]').get(0).click()
-            
-            expect(scope.sessionCount).to.equal(130);
+            $testRegion.find('input').val(130);
+            angular.element($testRegion.find('input')).triggerHandler('change');
         });
         describe('and then submittting the change', function(){
+            it('should set the sessionCount to the scope', function(){
+                var $form = angular.element($testRegion.find('form'))
+                $form.triggerHandler('submit');
 
+                expect(scope.sessionCount).to.equal(130);
+            });
         });
         describe('and then cancelling the change', function(){
+            it('should set the sessionCount to the scope', function(){
+                $("button[type='button']").trigger('click')
 
+                expect(scope.sessionCount).to.equal(this.sessionCount);
+            });
         });
     });
     describe('when changing the repeat frequency', function(){
+
+        // Reference for <select> element
+        // xeditable changes values to [0, 1, 2, 3] in DOM
+
+        //     [{value: 'w', text: 'week'},
+        //     {value: 'd', text: 'day'},
+        //     {value: null, text: 'once'},
+        //     {value: -1, text: 'forever'}];
+        var $repeatFrequency;
         beforeEach(function(){
-            var $repeatFrequency = $testRegion.find('.repeat-frequency');
+            $repeatFrequency = $testRegion.find('.repeat-frequency');
             $repeatFrequency.get(0).click();
         });
         describe('when changing it to days', function(){
-            it('should set the session count to 2', function(){
-
+            beforeEach(function(){
+                $testRegion.find('select').val('1');
+                angular.element($testRegion.find('select')).triggerHandler('change');
             });
-        });
-        describe('when changing it to weeks', function(){
-            it('should set the session count to 2', function(){
+            it('should set not reset the sessionCount', function(){
+                expect(scope.sessionCount).to.equal(this.sessionCount);
+            });
+            it('should set the repeatFrequency to "d"', function(){
+                expect(scope.repeatFrequency).to.equal("d")
+            });
+            describe('and then changing it back to weeks', function(){
+                beforeEach(function(){
+                    $repeatFrequency.get(0).click();
 
+                    $testRegion.find('select').val('0');
+                    angular.element($testRegion.find('select')).triggerHandler('change');
+                });
+                it('should set not reset the sessionCount', function(){
+                    expect(scope.sessionCount).to.equal(this.sessionCount);
+                });
+                it('should set the repeatFrequency to "w"', function(){
+                    expect(scope.repeatFrequency).to.equal("w")
+                });
             });
         });
         describe('when changing it to once', function(){
+            beforeEach(function(){
+                $testRegion.find('select').val('2');
+                angular.element($testRegion.find('select')).triggerHandler('change');
+            });
             it('should set the session count to null', function(){
+                expect(scope.sessionCount).to.equal(null);
+            });
+            it('should set the repeatFrequency to null', function(){
+                expect(scope.repeatFrequency).to.equal(null)
+            });
+            describe('and then changing it back to days', function(){
+                beforeEach(function(){
+                    $repeatFrequency.get(0).click();
 
+                    $testRegion.find('select').val('1');
+                    angular.element($testRegion.find('select')).triggerHandler('change');
+                });
+                it('should reset the sessionCount to 2', function(){
+                    expect(scope.sessionCount).to.equal(2);
+                });
+                it('should set the repeatFrequency to "d"', function(){
+                    expect(scope.repeatFrequency).to.equal("d")
+                });
             });
         });
         describe('when changing it to forever', function(){
-            it('should set the session count to -1', function(){
+            beforeEach(function(){
+                $testRegion.find('select').val('3');
+                angular.element($testRegion.find('select')).triggerHandler('change');
+            });
+            it('should set the session count to 1', function(){
+                expect(scope.sessionCount).to.equal(-1);
+            });
+            it('should set the repeatFrequency to -1', function(){
+                expect(scope.repeatFrequency).to.equal(-1)
+            });
+            describe('and then changing it back to weeks', function(){
+                beforeEach(function(){
+                    $repeatFrequency.get(0).click();
 
+                    $testRegion.find('select').val('0');
+                    angular.element($testRegion.find('select')).triggerHandler('change');
+                });
+                it('should reset the sessionCount to 2', function(){
+                    expect(scope.sessionCount).to.equal(2);
+                });
+                it('should set the repeatFrequency to "w"', function(){
+                    expect(scope.repeatFrequency).to.equal("w")
+                });
             });
         });
     });
