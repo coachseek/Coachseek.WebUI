e61d90b1841a847e5e0a92bd47b29ec197996b32
Reade Lobdill <readelobdill@gmail.com>
2014-12-19 16:46:00 -0800

    improve indentation

--- src/modules/app/app.js
+++ src/modules/app/app.js
@@ -6,7 +6,6 @@ angular.module('app',
   	'ui.bootstrap',
     'ui.router',
     'jm.i18next',
-    'xeditable',
 
     // coachSeekApp
     'app.controllers', 
@@ -19,21 +18,19 @@ angular.module('app',
     // UTILITIES
     'ngActivityIndicator'
 
-  ]).config(['$stateProvider', function ($stateProvider){
-    $stateProvider.state('home', { url: "/" });
-  }]).config(['$i18nextProvider', function( $i18nextProvider ){
-
-    $i18nextProvider.options = {
-        lng: 'en',
-        fallbackLng: 'en',
-        ns : {
-            namespaces : ['app', 'businessSetup'],
-            defaultNs: 'app'
-        },
-        resGetPath: 'modules/__ns__/i18n/__lng__/__ns__.json',
-        defaultLoadingValue: ''
-    };
-
+    ]).config(['$stateProvider', function ($stateProvider){
+        $stateProvider.state('home', { url: "/" });
+    }]).config(['$i18nextProvider', function( $i18nextProvider ){
+        $i18nextProvider.options = {
+            lng: 'en',
+            fallbackLng: 'en',
+            ns : {
+                namespaces : ['app', 'businessSetup'],
+                defaultNs: 'app'
+            },
+            resGetPath: 'modules/__ns__/i18n/__lng__/__ns__.json',
+            defaultLoadingValue: ''
+        };
     }]).run(['$rootScope', '$state', '$stateParams', 'editableOptions',
         function($rootScope, $stateParams, $state, editableOptions){
         $rootScope.$state = $state;
