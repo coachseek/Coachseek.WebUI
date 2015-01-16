ae0c26dc66430d35cab02143aaf44115deaa40a2
Reade Lobdill <readelobdill@gmail.com>
2015-01-15 20:28:11 -0800

    remove rest of time picker from time picker directive

--- src/modules/businessSetup/businessSetup.directives.js
+++ src/modules/businessSetup/businessSetup.directives.js
@@ -58,84 +58,11 @@ angular.module('businessSetup.directives', [])
 			templateUrl: 'businessSetup/partials/timeSlot.html'
 		};
 	})
-    .directive('timePicker', function(){
+    .directive('clockpicker', function(){
         return {
-            replace: true,
-            templateUrl: 'businessSetup/partials/timePicker.html',
-            scope: {
-                time: "="
-            },
-            link: function (scope, elem, attr) {
-
-                scope.$watch('time', function(newVal) {
-                    scope.time = newVal;
-                    if(scope.time){
-                        var timeArray = scope.time.split(":");
-                        scope.hours = parseFloat(timeArray[0]);
-                        scope.minutes = parseFloat(timeArray[1]);
-                    }
-                });
-     
-                /* Increases hours by one */
-                scope.increaseHours = function () {
-
-                    //Check whether hours have reached max
-                    if (scope.hours < 23) {
-                        scope.hours = ++scope.hours;
-                    }
-                    else {
-                        scope.hours = 0;
-                    }
-
-                    setTime();
-                };
-     
-                /* Decreases hours by one */
-                scope.decreaseHours = function () {
-     
-                    //Check whether hours have reached min
-                    scope.hours = scope.hours <= 0 ? 23 : --scope.hours;
-
-                    setTime();
-                };
-     
-                /* Increases minutes by 15 */
-                scope.increaseMinutes = function () {
-     
-                    //Check whether to reset
-                    if (scope.minutes >= 45) {
-                        scope.minutes = 0;
-                        scope.increaseHours();
-                    }
-                    else {
-                        scope.minutes = scope.minutes + 15;
-                    }
-                    setTime();
-                };
-     
-                /* Decreases minutes by 15 */
-                scope.decreaseMinutes = function () {
-     
-                    //Check whether to reset
-                    if (scope.minutes <= 0) {
-                        scope.minutes = 45;
-                        scope.decreaseHours();
-                    }
-                    else {
-                        scope.minutes = scope.minutes - 15;
-                    }
-                    setTime();
-                };
-
-                /* Displays minutes */
-                var displayMinutes = function () {
-                    return scope.minutes <= 9 ? "0" + scope.minutes : scope.minutes;
-                };
-
-                var setTime = function(){
-                    var minutesString = displayMinutes();
-                    scope.time = scope.hours + ":" + minutesString;
-                };
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                element.clockpicker(scope.$eval(attrs.clockpicker));
             }
         };
     })
