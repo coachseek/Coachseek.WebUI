b5011058a93906f8123c5958300f00368940eb63
Reade Lobdill <readelobdill@gmail.com>
2015-02-05 10:32:12 -0800

    [refs #2] update karma.config file

--- karma.config.js
+++ karma.config.js
@@ -32,20 +32,23 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'src/js/scripts.js': ['coverage']
-    },
+    // preprocessors: {
+    //   'src/js/scripts.js': ['coverage']
+    // },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'coverage'],
+    reporters: [
+        'dots',
+        // 'coverage'
+    ],
 
-    coverageReporter: { 
-        type : 'html',
-        dir : 'reports/coverage'
-    },
+    // coverageReporter: {
+    //     type : 'html',
+    //     dir : 'reports/coverage'
+    // },
 
 
     // web server port
