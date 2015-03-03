3206cf922ef5091f04b2d705a0c80f14b7b7b63b
Reade Lobdill <readelobdill@gmail.com>
2015-03-03 14:06:45 -0800

    basic customer functionality

--- src/modules/customers/customers.controllers.js
+++ src/modules/customers/customers.controllers.js
@@ -1,5 +1,32 @@
 angular.module('customers.controllers', [])
-    .controller('customersCtrl', ['$scope', '$q', 'coachSeekAPIService',
-        function($scope, $q, coachSeekAPIService){
+    .controller('customersCtrl', ['$scope', 'CRUDService',
+    	function($scope, CRUDService){
 
+    	$scope.createItem = function(){
+    	    $scope.newItem = true;
+    	    $scope.item = {};
+    	};
+
+    	$scope.editItem = function(customer){
+    	    _.pull($scope.itemList, customer);
+    	    $scope.itemCopy = angular.copy(customer);
+
+    	    $scope.item = customer;
+    	};
+
+    	$scope.saveItem = function(customer){
+    	    var formValid = CRUDService.validateForm($scope);
+
+    	    if(formValid){
+    	        CRUDService.update('Customers', $scope, customer);
+    	    }
+    	};
+
+    	$scope.cancelEdit = function(){
+    	    CRUDService.cancelEdit($scope);
+    	};
+
+    	$scope.itemList = [];
+    	$scope.createItem();
+    	// CRUDService.get('Customers', $scope);
     }]);
