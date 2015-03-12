403e277bc3e07199f6b2977ae7e94f4d1c834002
Reade Lobdill <readelobdill@gmail.com>
2015-03-12 13:26:25 -0700

    stop tests from trying to loadMore() because of scrolling

--- src/modules/customers/customers.controllers.js
+++ src/modules/customers/customers.controllers.js
@@ -36,9 +36,11 @@ angular.module('customers.controllers', [])
         $scope.alphabetLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","ALL"];
 
         $scope.loadMore = function() {
-            _.forEach(peopleList.shift(), function(person){
-                $scope.customerList.push(person);
-            });
+            if(peopleList){
+                _.forEach(peopleList.shift(), function(person){
+                    $scope.customerList.push(person);
+                });
+            }
         };
 
         var filterText = function(){
