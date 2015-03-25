1cd280b9c961f505deb96d65c8d3b048164fe24d
Reade Lobdill <readelobdill@gmail.com>
2015-03-25 14:51:33 -0700

    change loginModal to login to avoid confusion

--- src/modules/app/app.controllers.js
+++ src/modules/app/app.controllers.js
@@ -51,7 +51,7 @@ angular.module('app.controllers', [])
                 });
             };
 
-            $rootScope.loginModal = function(){
+            $rootScope.login = function(){
                 loginModal().then(function () {
                     $rootScope.removeAlerts();
                     $state.go('scheduling');
