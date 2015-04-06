f67fa213d9ec86ff2f79425ea33cfcd2e575a3c8
DaWyz <alban.escalier@gmail.com>
2015-04-07 00:13:24 +1200

    Changing navigation, now use angular state
    The first page has been removed (select between private and public session)
    Now, you have to select a location first, then a service, then you search for courses available for these filters
    Courses are now displayed, when you click on a course, you will see the list of sessions associated

--- src/modules/booking/booking.controllers.js
+++ src/modules/booking/booking.controllers.js
@@ -1,104 +1,113 @@
-angular.module('booking.controllers', [])
-  .controller('bookingCtrl', ['$scope', '$location', '$q', 'coachSeekAPIService', function ($scope, $location, $q, coachSeekAPIService) {
+angular
+.module('booking.controllers', [])
+.controller('bookingCtrl', function ($scope, $location, $q, $state, coachSeekAPIService) {
+  var savedCustomer = localStorage.getItem('customer');
 
-    var savedCustomer = localStorage.getItem('customer');
+  $scope.booking = {
+    customer: savedCustomer ? JSON.parse(savedCustomer) : null,
+    session: null
+  };
 
-    $scope.booking = {
-      session: null,
-      customer: savedCustomer ? JSON.parse(savedCustomer) : null,
-      enquiry: null
+  $scope.course = null;
+
+  $scope.filters = {
+    location: "",
+    service: ""
+  };
+
+  $q.all({
+    locations: coachSeekAPIService.get({section: 'Locations'}).$promise,
+    services: coachSeekAPIService.get({section: 'Services'}).$promise
+  })
+  .then(function (response) {
+    $scope.locations = response.locations;
+    if (response.locations.length === 1) {
+      $scope.filters.location = response.locations[0].id;
+    }
+
+    $scope.services  = response.services;
+    if (response.services.length === 1) {
+      $scope.filters.service = response.services[0].id;
+    }
+  },function (error) {
+    $scope.handleErrors(error);
+  });
+
+  var filterSessions = function (filters) {
+    var params = {
+      startDate: moment().format('YYYY-MM-DD'),
+      endDate: moment().add(12, 'week').format('YYYY-MM-DD'),
+      locationId: filters.location || undefined,
+      serviceId: filters.service || undefined,
+      section: 'Sessions'
     };
 
-    $scope.step = 'select';
-    $scope.navigate = {
-      to: function (step) {
-        var whiteList = ['select', 'booking', 'enquiry', 'register', 'confirm'];
+    return coachSeekAPIService.get(params).$promise;
+  };
 
-        if (!_.contains(whiteList, step)) {
-          throw new Error(step + ' is not a valid step.');
-        }
-
-        $scope.step = step;
-      },
-      back: function () {
-        if (_.contains(['booking', 'enquiry'], $scope.step)) {
-          return $scope.step = 'select';
-        }
-
-        if ($scope.step === 'register') {
-          if ($scope.booking.session !== null) {
-            return $scope.step = 'booking';
-          }
-
-          return $scope.step = 'enquiry';
-        }
-
-        if ($scope.step === 'confirm') {
-          $scope.step = 'select';
-        }
-      },
-      forward: function () {
-        if (_.contains(['booking', 'enquiry'], $scope.step)) {
-          if ($scope.booking.customer !== null) {
-            return $scope.step = 'confirm';
-          }
-
-          return $scope.step = 'register';
-        }
-
-        if ($scope.step === 'register') {
-          return $scope.step = 'confirm';
-        }
-      }
-    };
-
-    var startBookingLoading = function(){
-        if(!$scope.bookingLoading){
-            $scope.bookingLoading = true;
-        }
-    };
-
-    var stopBookingLoading = function(view, $element){
-        $scope.bookingLoading = false;
-    };
-
-    var filterSessions = function () {
-      var params = {
-        startDate: moment().format('YYYY-MM-DD'),
-        endDate: moment().add(1, 'month').format('YYYY-MM-DD'),
-        locationId: $scope.location || undefined,
-        serviceId: $scope.service || undefined,
-        section: 'Sessions'
-      };
-
-      return coachSeekAPIService.get(params).$promise;
-    };
-
-    $scope.filterSessions = function () {
-      filterSessions().then(function (sessions) {
-        $scope.sessions = sessions;
+  $scope.filterSessions = function (filters) {
+    filterSessions(filters).then(function (sessions) {
+      var courses = _.groupBy(sessions, function (session) {
+        return session.parentId;
       });
-    };
 
-    startBookingLoading();
-    $q.all({
-            locations: coachSeekAPIService.get({section: 'Locations'}).$promise,
-            services: coachSeekAPIService.get({section: 'Services'}).$promise,
-            sessions: filterSessions()
-        })
-        .then(function(response) {
-            $scope.locations = response.locations;
-            $scope.services  = response.services;
-            $scope.sessions = response.sessions;
-        },function(error){
-            $scope.handleErrors(error);
-            stopBookingLoading();
+      $scope.courses = Object.keys(courses).map(function (parentId) {
+        var course = courses[parentId][0];
+        return {
+          id: parentId,
+          name: course.service.name,
+          duration: course.timing.duration,
+          location: course.location.name,
+          color: course.presentation.colour,
+          sessions: courses[parentId]
+        };
+      });
+    });
+  };
+
+  $scope.selectCourse = function (course) {
+    $scope.course = course;
+    $state.go('booking.session');
+  };
+
+  $scope.selectSession = function (sessions) {
+    $scope.booking.sessions = sessions.filter(function (session) { return session.selected === true; });
+    if ($scope.booking.customer) {
+      $state.go('booking.confirm');
+    } else {
+      $state.go('booking.register');
+    }
+  };
+
+  $scope.cancel = function () {
+    $scope.booking.session = null;
+    $scope.booking.fullCourse = false;
+    $scope.booking.enquiry = null;
+    $scope.course = null;
+    $state.go('booking.course');
+  };
+
+  $scope.confirmBooking = function (booking) {
+    coachSeekAPIService
+      .update({ section: 'Customers' }, booking.customer).$promise
+      .then(function (customer) {
+        if (booking.customer.remember === true) {
+          localStorage.setItem('customer', JSON.stringify(customer));
+        }
+
+        booking.customer = customer;
+
+        coachSeekAPIService
+        .update({ section: 'Bookings' }, booking).$promise
+        .then(function (booking) {
+          console.log('your booking has been taken into consideration...');
+          console.log(booking);
         });
-  }])
-  .controller('registerCtrl', ['$scope', '$location', '$q', 'coachSeekAPIService', function ($scope, $location, $q, coachSeekAPIService) {
-    $scope.cancel = function () {
-      $location.path('/booking');
-    };
+      });
+  };
 
+  $scope.register = function (customer) {
 
-  }]);
+    $state.go('booking.confirm');
+  };
+});
--- src/modules/booking/booking.js
+++ src/modules/booking/booking.js
@@ -1,16 +1,43 @@
-angular.module('booking',
-    [
-        'booking.controllers',
-        'booking.directives',
-    ])
-    .config(['$stateProvider', function ($stateProvider) {
-        $stateProvider
-            .state('booking', {
-                url: "/booking",
-                templateUrl: "booking/partials/bookingView.html",
-                controller: 'bookingCtrl',
-                data: {
-                    requireLogin: true
-                }
-            });
-    }]);
+angular
+.module('booking', [ 'booking.controllers' ])
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('booking', {
+    url: "/booking",
+    templateUrl: "booking/partials/layout.html",
+    controller: 'bookingCtrl',
+    data: {
+      requireLogin: true
+    }
+  })
+  .state('booking.course', {
+    url: "/course",
+    templateUrl: "booking/partials/booking.course.view.html",
+    data: {
+      requireLogin: true
+    }
+  })
+  .state('booking.session', {
+    url: "/session",
+    templateUrl: "booking/partials/booking.session.view.html",
+    data: {
+      requireLogin: true
+    }
+  })
+  .state('booking.register', {
+    url: "/register",
+    templateUrl: "booking/partials/booking.register.view.html",
+    data: {
+      requireLogin: true
+    }
+  })
+  .state('booking.confirm', {
+    url: "/confirm",
+    templateUrl: "booking/partials/booking.confirm.view.html",
+    data: {
+      requireLogin: true
+    }
+  });
+
+  $urlRouterProvider.otherwise('/booking/session');
+});
