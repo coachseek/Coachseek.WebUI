33ad8b9c5f186f7ab20ea72d74ba26aefc578093
Reade Lobdill <readelobdill@gmail.com>
2015-06-22 15:56:28 -0700

    [#96667326] remove unnecessary payment view

--- src/modules/booking/booking.controllers.js
+++ src/modules/booking/booking.controllers.js
@@ -172,27 +172,6 @@ angular.module('booking.controllers', [])
             $state.go('booking.selection');
         }
     }])
-    .controller('bookingPaymentCtrl', ['$scope', function($scope){
-      // GARRET PLAYS HERE
-      //TODO: Grab booking detail data
-      //and attach to the hidden forms as ng-values..
-      
-      //inititate paypal processing
-      //I don't think we'll need a post here...
-      $scope.initPaypalPayment = function () {
-        if ($scope.coursePaymentPrice === null) {
-          return;
-        } else {
-          //TODO: Post to paypal return callback to confirmation page
-        }
-            
-      };
-      
-      //Post booking as unpayed
-      $scope.payLaterCall = function () {
-        
-      };
-    }])
     .controller('bookingConfirmationCtrl', ['$scope', '$q', '$state', 'onlineBookingAPIFactory', 'currentBooking',
       function($scope, $q, $state, onlineBookingAPIFactory, currentBooking){
         $scope.bookingConfirmed = false;
