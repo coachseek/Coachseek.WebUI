ae2e23f5b363938b131c9215c2e2e5a96aa29316
Reade Lobdill <readelobdill@gmail.com>
2015-07-07 18:29:44 -0700

    Revert "remove template.js file"
    This reverts commit 6dc73cf6cc5c0120122c681682310b0a0978b908.

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,6 +61,27 @@ module.exports = function(grunt) {
                 ],
             },
         },
+        concat: {
+            buildCss: {
+                src: ['src/modules/**/css/*.scss'],
+                dest: 'build/css/style.css'
+            },
+            srcCss: {
+                src: ['src/modules/**/css/*.scss'],
+                dest: 'src/css/style.css'
+            },
+            srcApp: {
+                src: [
+                    'src/modules/**/*.js',
+                    '!src/modules/**/*.spec.js'
+                ],
+                dest: 'src/js/scripts.js'
+            },
+            srcLibs: {
+                src: ['src/libs/*.js'],
+                dest: 'src/js/libs.js'
+            }
+        },
         wrap:{
             scripts: {
                 src: ['src/js/scripts.js'],
@@ -87,19 +108,13 @@ module.exports = function(grunt) {
                 options:{
                     module:'app',         // (Optional) The module the templates will be added to
                     htmlmin: {
-                        collapseBooleanAttributes:      true,
-                        collapseWhitespace:             true,
-                        removeAttributeQuotes:          true,
-                        removeComments:                 true, // Only if you don't use comment directives! 
-                        removeEmptyAttributes:          true,
-                        removeRedundantAttributes:      true,
-                        removeScriptTypeAttributes:     true,
-                        removeStyleLinkTypeAttributes:  true
+                        collapseWhitespace: true,
+                        collapseBooleanAttributes: true
                     }
                 },
                 cwd: 'src/modules',
                 src:'**/partials/*.html',
-                dest:'build/js/scripts.js'
+                dest:'build/js/templates.js'
             },
             src:{
                 options:{
@@ -108,7 +123,7 @@ module.exports = function(grunt) {
                 },
                 cwd: 'src/modules',
                 src: '**/partials/*.html',
-                dest: 'src/js/scripts.js'
+                dest: 'src/js/templates.js'
             },
             test: {
                 options:{
@@ -119,34 +134,11 @@ module.exports = function(grunt) {
                 dest: 'src/js.spec/testTemplates.js'
             }
         },
-        concat: {
-            buildCss: {
-                src: ['src/modules/**/css/*.scss'],
-                dest: 'build/css/style.css'
-            },
-            srcCss: {
-                src: ['src/modules/**/css/*.scss'],
-                dest: 'src/css/style.css'
-            },
-            srcApp: {
-                src: [
-                    'src/modules/**/*.js',
-                    'src/js/scripts.js', // templates living here temporarily
-                    '!src/modules/**/*.spec.js'
-                ],
-                dest: 'src/js/scripts.js'
-            },
-            srcLibs: {
-                src: ['src/libs/*.js'],
-                dest: 'src/js/libs.js'
-            }
-        },
         //TODO - switch to ng-annotate?
         uglify: {
             buildApp: {
                 src: [
                     'src/modules/**/*.js',
-                    'build/js/scripts.js', // templates living here temporarily
                     '!src/modules/**/*.spec.js'
                 ],
                 dest: 'build/js/scripts.js'
@@ -260,10 +252,10 @@ module.exports = function(grunt) {
 
     // Default task(s).
     grunt.registerTask('default', [
+            'concat',
             'wrap',
             'htmlmin',
             'ngtemplates',
-            'concat',
             'uglify',
             'sass',
             'merge-json',
