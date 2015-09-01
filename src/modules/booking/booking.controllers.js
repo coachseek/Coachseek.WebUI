833b58463fd1872464952010b4ebeec51e1c225f
Reade Lobdill <readelobdill@gmail.com>
2015-09-01 15:40:42 -0700

    [#101939220] add heap tracking for user payment during online booking

--- src/modules/booking/booking.controllers.js
+++ src/modules/booking/booking.controllers.js
@@ -202,7 +202,7 @@ angular.module('booking.controllers', [])
                 .save({ section: 'Customers' }, currentBooking.customer).$promise
                     .then(function (customer) {
                         return saveBooking(customer).then(function (booking) {
-                            heap.track('Online Booking Confirmed');
+                            heap.track('Online Booking Confirmed', {onlinePaymentEnabled: $scope.business.payment.isOnlinePaymentEnabled, payLater: payLater});
                             currentBooking.booking.id = booking.id;
                             $scope.bookingConfirmed = payLater;
                             $scope.redirectingToPaypal = !payLater;
