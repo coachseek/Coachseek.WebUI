4d131f7780bf770381eb09f3ee56de394b5d4a6c
sOm2y <yyin888@aucklanduni.ac.nz>
2015-11-06 16:02:08 +1300

    [#107358310]check app is first launch

--- src/modules/app/app.js
+++ src/modules/app/app.js
@@ -60,7 +60,13 @@ angular.module('app',
             $rootScope.$stateParams = $stateParams;
 
             $rootScope.alerts = [];
-
+            // $window.localStorage.clear();
+            var applaunchCount = $window.localStorage.getItem('launchCount');
+            if(!applaunchCount){               
+                $state.go("mobileOnboaringSignUp");
+                $window.localStorage.setItem('launchCount',1);
+            }
+            
             $window.fbAsyncInit = function() {
                 FB.init({
                   appId      : '172144976459129',
