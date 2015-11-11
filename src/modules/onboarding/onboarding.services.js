e2791f6fb89096adf6a49e8488f4b48122a21b69
sOm2y <yyin888@aucklanduni.ac.nz>
2015-11-12 12:50:11 +1300

    [#107782590]create mobile onboarding modal service

--- src/modules/onboarding/onboarding.services.js
+++ src/modules/onboarding/onboarding.services.js
@@ -14,4 +14,20 @@ angular.module('onboarding.services', [])
                 }
             }
         }
+    ])
+    .service('mobileOnboardingSkipModal', ['$modal', '$rootScope',
+        function ($modal, $rootScope) {
+            return {
+                open: function(template, ctrl) {
+                    var instance = $modal.open({
+                        templateUrl: 'onboarding/partials/' + template + '.html',
+                        controller: ctrl,
+                        windowClass: 'm-onboarding-skip-modal-backdrop',
+                        backdrop: 'static'
+                    });
+
+                    return instance.result;
+                }
+            }
+        }
     ]);
