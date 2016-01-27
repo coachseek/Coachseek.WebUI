bae8aec0ccc78c2d21c45125c1e1da8545f338a0
Reade Lobdill <readelobdill@gmail.com>
2016-01-27 16:32:32 +1300

    [#112484293] update calendar events independently of currentEvent
    was previously relying on currentEvent to update courseModal but the currentEvent may not necessarily be the same as when the booking was updated due to async requests.

--- src/modules/scheduling/scheduling.services.js
+++ src/modules/scheduling/scheduling.services.js
@@ -36,62 +36,75 @@ angular.module('scheduling.services', [])
         function($q, coachSeekAPIService, uiCalendarConfig, currentEventService){
 
         this.addToCourse = function(customer){
+            var courseId = currentEventService.event.course.id;
             return coachSeekAPIService.save({section: 'Bookings'}, buildBooking(customer))
                 .$promise.then(function(booking){
-                    return getUpdatedEvent(booking);
+                    return updateCourse(courseId).then(function(){
+                        return booking;
+                    });
                 });
         };
 
         this.addToSession = function(customer, sessionId){
+            var eventUpdate = getEventUpdateInfo();
             return coachSeekAPIService.save({section: 'Bookings'}, buildBooking(customer, sessionId))
                 .$promise.then(function(booking){
-                    return getUpdatedEvent(booking);
+                    return eventUpdate.updateEventFunction(eventUpdate.eventId).then(function(){
+                        return booking;
+                    });
                 });
         }
 
         this.updateBooking = function(bookingId, updateCommand){
+            var eventUpdate = getEventUpdateInfo();
             return coachSeekAPIService.save({section: 'Bookings', id: bookingId}, updateCommand)
                 .$promise.then(function(booking){
-                    return getUpdatedEvent(booking)
+                    return eventUpdate.updateEventFunction(eventUpdate.eventId).then(function(){
+                        return booking;
+                    });
                 });
         };
 
         this.removeFromSession = function(bookingId){
+            var eventUpdate = getEventUpdateInfo();
             return deleteBooking(bookingId).then(function(){
-                return getUpdatedEvent();
+                return eventUpdate.updateEventFunction(eventUpdate.eventId);
             });
+        }
+
+        function getEventUpdateInfo(){
+            if(currentEventService.event.course){
+                return {
+                    eventId: currentEventService.event.course.id,
+                    updateEventFunction: updateCourse
+                }
+            } else {
+                return {
+                    eventId: currentEventService.event.session.id,
+                    updateEventFunction: updateStandaloneSession
+                }
+            }
         }
 
         //accepts all courseBookings. deletes using $q.all
         this.removeFromCourse = function(courseBookings){
+            var courseId = currentEventService.event.course.id;
             var bookingsToDelete = [];
             _.each(courseBookings, function(booking){
                 bookingsToDelete.push(deleteBooking(booking.parentId));
             });
             return $q.all(bookingsToDelete).then(function(bookings){
-                return getUpdatedEvent();
+                return updateCourse(courseId);
             });
         }
 
-        function getUpdatedEvent(booking){
-            if(currentEventService.event.course){
-                //Update session and course
-                return updateCourse().then(function(){
-                    return booking;
-                });
-            } else {
-                //Update standalone session
-                return updateStandaloneSession().then(function(){
-                    return booking;
-                });
-            }
-        };
-
-        function updateCourse(){
-            return getUpdatedEvents(currentEventService.event.course.id).then(function(course){
-                _.each(currentEventService.currentCourseEvents , function(courseEvent){
+        function updateCourse(courseId){
+            //courseId must be grabbed before this oint from currentEventService in case currentEvent changes during async
+            return getUpdatedEvents(courseId).then(function(course){
+                var courseEvents = getCalendarEventsByCourseId(course.id);
+                _.each(courseEvents, function(courseEvent){
                     //fullcalendar needs original event so we get it from the calendar here
-                    var event = uiCalendarConfig.calendars.sessionCalendar.fullCalendar('clientEvents', courseEvent._id)[0];
+                    var event = courseEvent;
                     _.assign(event, {
                         session: _.find(course.sessions, function(session){return session.id === event.session.id}),
                         course:  course
@@ -100,14 +113,26 @@ angular.module('scheduling.services', [])
             })
         }
 
-        function updateStandaloneSession(){
-            return getUpdatedEvents(currentEventService.event.session.id).then(function(session){
+        function getCalendarEventsByCourseId(courseId){
+            return _.filter(uiCalendarConfig.calendars.sessionCalendar.fullCalendar('clientEvents'), function(event){
+                return _.get(event, 'course.id', 1) === courseId;
+            });
+        }
+
+        function updateStandaloneSession(sessionId){
+            //sessionId must be grabbed before this oint from currentEventService in case currentEvent changes during async
+            return getUpdatedEvents(sessionId).then(function(session){
                 //fullcalendar needs original event so we get it from the calendar here
-                var event = uiCalendarConfig.calendars.sessionCalendar.fullCalendar('clientEvents', currentEventService.event._id)[0];
+                var event = getCalendarEventBySessionId(session.id);
                 event.session = session;
             });
         }
 
+        function getCalendarEventBySessionId(sessionId){
+            return _.find(uiCalendarConfig.calendars.sessionCalendar.fullCalendar('clientEvents'), function(event){
+                return _.get(event, 'session.id', 1) === sessionId;
+            });
+        }
 
         function getUpdatedEvents(id){
             return coachSeekAPIService.get({section: 'Sessions', id: id}).$promise;
