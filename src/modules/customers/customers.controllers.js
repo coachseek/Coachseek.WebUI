1d29b5ffe75e811ded1d1ec15717b1eb6e349baa
Reade Lobdill <readelobdill@gmail.com>
2016-02-05 12:50:53 +1300

    [#96670218] only show isActive notes on customer list until we get the non active ones returning

--- src/modules/customers/customers.controllers.js
+++ src/modules/customers/customers.controllers.js
@@ -87,7 +87,7 @@ angular.module('customers.controllers', [])
         }).then(function(response) {
             $scope.itemList = response.customers;
 
-            $scope.customerNotes = response.customerNotes;
+            $scope.customerNotes = _.filter(response.customerNotes, function(note) { return note.isActive; });
             var customFields = [];
             _.each($scope.customerNotes, function(note){customFields.push(note.key); });
             $scope.exportKeys.push({customFields: customFields});
